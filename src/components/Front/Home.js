4ab39b4a371d3e02bf53cab105b6352d9e8c5844
TiagoLopes13 <tiagolopes8172@escolasdesatao.pt>
2023-04-20 16:55:07 +0100

    123456

--- src/components/Front/Home.js
+++ src/components/Front/Home.js
@@ -35,14 +35,14 @@ function Home() {
 
             <div class="mt-8 flex flex-wrap gap-4 text-center">
               <a
-                href="#cozinhas"
+                href="#start"
                 class="block w-full scroll-smooth rounded bg-tq4 px-12 py-3 text-sm font-medium text-white shadow transition hover:bg-tq2 focus:outline-none focus:ring active:bg-tq2 sm:w-auto"
               >
                 Vamos começar
               </a>
 
               <a
-                href="#catalogo"
+                href="#find"
                 class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-tq1 shadow hover:text-tq2 focus:outline-none focus:ring active:text-tq2 sm:w-auto"
               >
                 Saber Mais
@@ -52,7 +52,7 @@ function Home() {
         </div>
       </section>
 
-      <div id="cozinhas">
+      <div id="start">
         <div class="bg-white">
           <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-5xl text-center">
@@ -87,13 +87,16 @@ function Home() {
           className="object-cover object-center"
         />
       </div>
-      <h1 class="mt-2 text-3xl font-extrabold text-tq1 sm:text-4xl mb-3 text-center">
+      <h1
+        class="mt-2 text-3xl font-extrabold text-tq1 sm:text-4xl mb-3 text-center"
+        id="find"
+      >
         Descobre mais +
       </h1>
       <section className="bg-tq1">
         <div class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:px-8">
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
-            <div class="grid place-content-center rounded bg-gray-100 p-6 sm:p-8">
+            {/* <div class="grid place-content-center rounded bg-gray-100 p-6 sm:p-8">
               <div class="mx-auto max-w-md text-center lg:text-left">
                 <header>
                   <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">
@@ -105,17 +108,32 @@ function Home() {
                     de encontro às suas espectativas
                   </p>
                 </header>
-                {/* <Link
+                <Link
                   to="productsList"
                   class="mt-8 inline-block rounded border border-gray-900 bg-gray-900 px-12 py-3 text-sm font-medium text-white transition hover:shadow focus:outline-none focus:ring"
                 >
                   <h1>Veja aqi!</h1>
-                </Link> */}
+                </Link>
               </div>
-            </div>
+            </div> */}
 
-            <div class="lg:col-span-2 lg:py-8">
-              <ul class="grid grid-cols-2 gap-4">
+            <div class="lg:col-span-3 lg:py-8">
+              <ul class="grid grid-cols-3 gap-8">
+                <li>
+                  {/* <Link to="#" class="group block"> */}
+                  <img
+                    src="https://www.bodybuilding.com/images/2021/march/what-is-the-best-5-day-workout-split-header-960x540.jpg"
+                    alt=""
+                    class="aspect-square w-full rounded object-cover"
+                  />
+
+                  <div class="mt-3">
+                    <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                      Cozinha Deluxe
+                    </h3>
+                  </div>
+                  {/* </Link> */}
+                </li>
                 <li>
                   {/* <Link to="#" class="group block"> */}
                   <img
@@ -133,19 +151,19 @@ function Home() {
                 </li>
 
                 <li>
-                  {/* <Link to="#" class="group block">
-                    <img
-                      src="https://www.eshoper.in/wp-content/uploads/2021/12/best-kitchen-furniture-latest-design-low-price-furnitures-store-panvel-kamothe-navi-mumbai.jpg"
-                      alt=""
-                      class="aspect-square w-full rounded object-cover"
-                    />
+                  {/* <Link to="#" class="group block"> */}
+                  <img
+                    src="https://www.eshoper.in/wp-content/uploads/2021/12/best-kitchen-furniture-latest-design-low-price-furnitures-store-panvel-kamothe-navi-mumbai.jpg"
+                    alt=""
+                    class="aspect-square w-full rounded object-cover"
+                  />
 
-                    <div class="mt-3">
-                      <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
-                        Cozinha Cerâmica
-                      </h3>
-                    </div>
-                  </Link> */}
+                  <div class="mt-3">
+                    <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                      Cozinha Cerâmica
+                    </h3>
+                  </div>
+                  {/* </Link> */}
                 </li>
               </ul>
             </div>
